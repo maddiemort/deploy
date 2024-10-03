f495d02b68e7d103a62b109307cf8a5211ad0a7a
Madeleine Mortensen <me@maddie.wtf>
2024-10-03 21:02:46 +0100

    feat: set up vexillologist-dev postgres table, fix both tables

--- modules/vexillologist.nix
+++ modules/vexillologist.nix
@@ -34,25 +34,46 @@ in
       inherit (cfg) enable;
       package = pkgs.postgresql_15;
 
-      ensureDatabases = [ "vexillologist" ];
+      ensureDatabases = [
+        "vexillologist"
+        "vexillologist-dev"
+      ];
+
       ensureUsers = [
         {
           name = "vexillologist";
           ensureDBOwnership = true;
         }
+        {
+          name = "vexillologist-dev";
+          ensureDBOwnership = true;
+        }
       ];
 
+      enableTCPIP = true;
+
       authentication = mkOverride 10 ''
-        # TYPE    DATABASE       USER        ADDRESS    METHOD    OPTIONS
-        local     sameuser       all                    peer      map=superuser_map
-        local     all            postgres               peer
-        local     replication    all                    peer
+        # TYPE    DATABASE       USER                 ADDRESS              METHOD    OPTIONS
+        local     sameuser       all                                       peer      map=superuser_map
+        local     all            postgres                                  peer
+        local     replication    all                                       peer
+        host      sameuser       vexillologist        100.114.76.116/32    trust
+        host      sameuser       vexillologist        100.85.114.74/32     trust
+        host      sameuser       vexillologist-dev    100.114.76.116/32    trust
+        host      sameuser       vexillologist-dev    100.85.114.74/32     trust
       '';
       identMap = ''
         # ArbitraryMapName SystemUser DBUser
         superuser_map      root       postgres
         superuser_map      postgres   postgres
+        superuser_map      maddie     vexillologist
+        superuser_map      maddie     vexillologist-dev
         superuser_map      /^(.*)$    \1
+      '';
+
+      initialScript = pkgs.writeText "postgres-init-script" ''
+        GRANT ALL ON SCHEMA public TO vexillologist;
+        GRANT ALL ON SCHEMA public TO vexillologist-dev;
       '';
     };
   };
