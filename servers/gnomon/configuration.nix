d30850d74ff063684d8c93183d776b5a4a799f54
Madeleine Mortensen <me@maddie.wtf>
2025-08-11 21:21:34 +0100

    chore: add user for joe to gnomon

--- servers/gnomon/configuration.nix
+++ servers/gnomon/configuration.nix
@@ -15,6 +15,23 @@
 
   services.openssh.enable = true;
 
+  users.users.josephcryer = {
+    createHome = true;
+    description = "Joseph Cryer";
+    isNormalUser = true;
+    home = "/home/josephcryer";
+    group = "josephcryer";
+    extraGroups = [
+      "minecraft"
+    ];
+
+    openssh.authorizedKeys.keys = builtins.map builtins.readFile [
+      ../../keys/joe.pub
+    ];
+  };
+
+  users.groups.josephcryer = { };
+
   time.timeZone = "Europe/London";
 
   system.stateVersion = "22.11";
